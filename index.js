67a05ea4c41ec9736ae62fdaf82d661d0ae6b834
Rothanak So <rothanak.hello@gmail.com>
2019-05-08 17:23:22 -0400

    First deployment

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const Discord = require('discord.js');
+const dotenv = require('dotenv');
+dotenv.config();
+
 const client = new Discord.Client();
-const auth = require('./auth.json');
+
+const TOKEN = process.env.TOKEN;
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -12,4 +16,22 @@ client.on('message', msg => {
   }
 });
 
-client.login(auth.token);
+client.login(TOKEN);
+
+client.on('guildCreate', guild => {
+  let channelID;
+  let channels = guild.channels;
+  channelLoop:
+  for (let c of channels) {
+      let channelType = c[1].type;
+      if (channelType === "text") {
+          channelID = c[0];
+          break channelLoop;
+      }
+  }
+
+  let channel = client.channels.get(guild.systemChannelID || channelID);
+  channel.send(`Hello world!`);
+});
+
+require('http').createServer().listen(3000)
